a8b3306e15c9417d5e4c983eb37534a0573bfff8
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-02 21:53:46 +0600

    Update scroll-header.js

--- Elcho/js/scroll-header.js
+++ Elcho/js/scroll-header.js
@@ -6,3 +6,13 @@ $(window).scroll(function () {
 		$("#header__scroll").removeClass("small");
 	}
 });
+!(function (e, t) {
+	"use strict";
+	"object" == typeof module && "object" == typeof module.exports
+		? (module.exports = e.document
+				? t(e, !0)
+				: function (e) {
+						if (!e.document) throw new Error("jQuery requires a window with a document");
+						return t(e);
+				  })
+		: t(e);
