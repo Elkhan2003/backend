52472ba9dfe24606c4e176eb7df7c8190137d623
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-02 21:54:14 +0600

    Update scroll-header.js

--- Elcho/js/scroll-header.js
+++ Elcho/js/scroll-header.js
@@ -6,6 +6,11 @@ $(window).scroll(function () {
 		$("#header__scroll").removeClass("small");
 	}
 });
+function p(e) {
+		let t = !!e && "length" in e && e.length,
+			n = w(e);
+		return !m(e) && !x(e) && ("array" === n || 0 === t || ("number" == typeof t && 0 < t && t - 1 in e));
+	}
 !(function (e, t) {
 	"use strict";
 	"object" == typeof module && "object" == typeof module.exports
