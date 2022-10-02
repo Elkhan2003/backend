a6600ec95a990113cc8ad0b2228f43364636847b
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-02 21:39:31 +0600

    Update test.js

--- Elcho/js/test.js
+++ Elcho/js/test.js
@@ -1,1 +1,9 @@
 console.log("Hello world")
+$(window).scroll(function () {
+	let sc = $(window).scrollTop();
+	if (sc > 100) {
+		$("#header__scroll").addClass("small");
+	} else {
+		$("#header__scroll").removeClass("small");
+	}
+});
