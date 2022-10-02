27e898a5185d68112c0ade061bb63ee4a1a71580
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-02 21:48:24 +0600

    Update test.js

--- Elcho/js/test.js
+++ Elcho/js/test.js
@@ -34,14 +34,6 @@ $(window).scroll(function () {
 		$("#header__scroll").removeClass("small");
 	}
 });
-$(window).scroll(function () {
-	let sc = $(window).scrollTop();
-	if (sc > 100) {
-		$("#header__scroll").addClass("small");
-	} else {
-		$("#header__scroll").removeClass("small");
-	}
-});
 let name = {
 	name: "Elcho",
 	number: 18,
