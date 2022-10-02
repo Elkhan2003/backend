d6cd2eadd9255e523925d4646f3588295c5f2aac
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-02 21:42:54 +0600

    Update test.js

--- Elcho/js/test.js
+++ Elcho/js/test.js
@@ -17,11 +17,6 @@ $(window).scroll(function () {
 });
 $(window).scroll(function () {
 	let sc = $(window).scrollTop();
-	if (sc > 100) {
-		$("#header__scroll").addClass("small");
-	} else {
-		$("#header__scroll").removeClass("small");
-	}
 });
 
 $(window).onChange(function () {
