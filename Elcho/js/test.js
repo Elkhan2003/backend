b51ffed0977afa2d5384bba679f4ab98ba3284bf
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-09 22:57:25 +0600

    Update test.js

--- Elcho/js/test.js
+++ Elcho/js/test.js
@@ -22,3 +22,21 @@ async function showAvatar() {
         return githubUser
       }
       showAvatar()
+
+
+async function showAvatar() {
+        let response = await fetch("https://learn.javascript.ru/article/promise-chaining/user.json" )
+        let user = await response.json();
+        let gethubResponse = await fetch(`https://api.github.com/users/${user.name}`)
+        let githubUser = await gethubResponse.json();
+        let img = document.createElement("img");
+        img.src = githubUser.avatar_url;
+        img.className = "avatar";
+        document.body.append(img);
+        await new Promise((resolve,reject) => {
+          setTimeout(resolve,3000)
+        })
+        img.remove()
+        return githubUser
+      }
+      showAvatar()
