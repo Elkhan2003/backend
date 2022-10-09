77ff3d86c3131b6f9573077398ec5c95427a3d3a
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-09 22:59:39 +0600

    Update test.js

--- Elcho/js/test.js
+++ Elcho/js/test.js
@@ -37,7 +37,18 @@ const test = async () => {
         .then(res => res.json())
         .then(res => console.log(res))
       }
-      test()han2003`)
+      test()han2003`)const test = async () => {
+        let response = await fetch(`https://api.github.com/users/Elkhan2003`)
+        let user = await response.json()
+        console.log(user)
+      }
+      test()
+      function test() {
+        fetch(`https://api.github.com/users/Elkhan2003`)
+        .then(res => res.json())
+        .then(res => console.log(res))
+      }
+      test()
         let user = await response.json()
         console.log(user)
       }
