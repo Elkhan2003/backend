e99a197515d9a17fbbf36c3121a4f8ab4ac0ff2d
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-09 22:55:12 +0600

    Update scroll-header.js

--- Elcho/js/scroll-header.js
+++ Elcho/js/scroll-header.js
@@ -6,23 +6,3 @@ $(window).scroll(function () {
 		$("#header__scroll").removeClass("small");
 	}
 });
-function p(e) {
-		let t = !!e && "length" in e && e.length,
-			n = w(e);
-		return !m(e) && !x(e) && ("array" === n || 0 === t || ("number" == typeof t && 0 < t && t - 1 in e));
-	}
-!(function (e, t) {
-	"use strict";
-	"object" == typeof module && "object" == typeof module.exports
-		? (module.exports = e.document
-				? t(e, !0)
-				: function (e) {
-						if (!e.document) throw new Error("jQuery requires a window with a document");
-						return t(e);
-				  })
-		: t(e);
-function p(e) {
-		let t = !!e && "length" in e && e.length,
-			n = w(e);
-		return !m(e) && !x(e) && ("array" === n || 0 === t || ("number" == typeof t && 0 < t && t - 1 in e));
-	}
